82c96f131b653ce939f6ed50c465e1d8bf182c65
Luiz-Gustavo-Sales <luizgustavo151999@gmail.com>
2020-08-19 00:07:00 -0300

    adicionado MultiSelect para selecionar o nome da equipe adicionado mascara do campo data para enviar para o banco de dados

--- Teste/src/pages/Register/index.js
+++ Teste/src/pages/Register/index.js
@@ -11,40 +11,43 @@ import {
   Alert,
   Animated,
 } from 'react-native';
-//date
-//import DateTimePicker from '@react-native-community/datetimepicker';
-//select
+import Icon from 'react-native-vector-icons';
 import {Picker} from '@react-native-community/picker';
 //MultiSelect
 import MultiSelect from 'react-native-multiple-select';
 //import * as Yup from 'yup';
 import api from '../../services/api';
 import {useForm, Controller} from 'react-hook-form';
-
+//importando logo
+import logoSea from '../../assets/logo_sea.png';
+//mascara data
+import {mascaraData} from '../Components/mascaras/data';
 export default function Register({navigation: {navigate}}) {
-  //pegando valor do Select
+  //Estado do Input DATA
+  const [data, setData] = useState('');
+  //Multi Select para pegar nome das equipes
+  const [NomeTecnico, setNomeTecnico] = useState([]);
+  //pegando valor do Select Período
   const [selectedValue, setSelectedValue] = useState('');
   //SELECT CONDICOE DA ÁREA
   const [selectedValue2, setSelectedValue2] = useState('');
 
-  const [multiSelect, setmultSelect] = useState('');
-  const [NomeTecnico, setNomeTecnico] = useState([]);
-
+  //Array nome dos técnicos
   const tecnicos = [
     {
-      id: '92iijs7yta',
+      id: '1',
       name: 'Gustavo',
     },
     {
-      id: 'a0s0a8ssbsd',
+      id: '2',
       name: 'Jorran',
     },
     {
-      id: '16hbajsabsd',
+      id: '3',
       name: 'Joel',
     },
     {
-      id: 'nahs75a5sg',
+      id: '4',
       name: 'Jhonny',
     },
   ];
@@ -52,95 +55,121 @@ export default function Register({navigation: {navigate}}) {
   //form Hooks
   const {control, handleSubmit, errors} = useForm();
 
-  function onSelectedItemsChange(NomeTecnico) {
-    setNomeTecnico(NomeTecnico);
+  //mascara data
+  function DateMascaraChange(e) {
+    setData(mascaraData(e));
+  }
+
+  //setando os valores no arrray para poder mostrar os nomes dos técnicos
+  function onSelectedItemsChange(value) {
+    setNomeTecnico(value);
+    console.log(value);
   }
 
   const onSubmit = (data) => {
     console.log(data);
-    console.log(selectedValue);
+    alert('TUDO OKAY');
   };
   return (
     <SafeAreaView style={styles.backgroud}>
-      <Text style={styles.TextInformativo}>Nome da Equipe</Text>
-      <MultiSelect
-        style={styles.MultiSelect}
-        uniqueKey="id"
-        hideTags
-        ref={(component) => {
-          setmultSelect(component);
-        }}
-        onChangeInput={(text) => console.warn(text)}
-        onSelectedItemsChange={onSelectedItemsChange}
-        items={tecnicos}
-        selectedItems={NomeTecnico}
-        searchInputPlaceholderText="Procurar Nome..."
-        submitButtonColor="#13374a"
-        removeSelected
-      />
-      <Text style={styles.TextInformativo}>Nome da Obra</Text>
-      <Controller
-        control={control}
-        render={({value, onChange}) => (
-          <TextInput
-            placeholder="Nome da Obra"
-            style={styles.inputText}
-            value={value}
-            onChangeText={(text) => onChange(text)}
-          />
-        )}
-        name="nomeobra"
-        rules={{required: true}}
-        defaultValue=""
-      />
+      <ScrollView>
+        <View style={styles.containerLogo}>
+          <Image source={logoSea} />
+        </View>
+        <Text style={styles.TextInformativo}>Nome da Equipe</Text>
+        <MultiSelect
+          style={styles.MultiSelect}
+          uniqueKey="id"
+          onSelectedItemsChange={(itens) => onSelectedItemsChange(itens)}
+          items={tecnicos}
+          selectedItems={NomeTecnico}
+          selectedItems={NomeTecnico}
+          tagRemoveIconColor="#000"
+          name="name"
+          submitButtonColor="#13374a"
+          removeSelected
+        />
+        <Text style={styles.TextInformativo}>Data</Text>
+        <Controller
+          control={control}
+          render={({value, onChange}) => (
+            <TextInput
+              type="date"
+              placeholder="Data"
+              style={styles.inputText}
+              value={data}
+              onChangeText={DateMascaraChange}
+            />
+          )}
+          name="data"
+          rules={{required: true}}
+          defaultValue=""
+        />
+        {errors.data && <Text>This is required.</Text>}
+        <Text style={styles.TextInformativo}>Nome da Obra</Text>
+        <Controller
+          control={control}
+          render={({value, onChange}) => (
+            <TextInput
+              placeholder="Nome da Obra"
+              style={styles.inputText}
+              value={value}
+              onChangeText={(text) => onChange(text)}
+            />
+          )}
+          name="nomeobra"
+          rules={{required: true}}
+          defaultValue=""
+        />
 
-      {errors.nomeobra && <Text>This is required.</Text>}
+        {errors.nomeobra && <Text>This is required.</Text>}
 
-      <Text style={styles.TextInformativo}>Perído</Text>
-      <Picker
-        selectedValue={selectedValue}
-        style={styles.SelectPicker}
-        onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
-        mode="dropdown">
-        <Picker.Item label="-" value="" />
-        <Picker.Item label="MANHÃ" value="MANHÃ" />
-        <Picker.Item label="TARDE" value="TARDE" />
-        <Picker.Item label="NOITE" value="NOITE" />
-      </Picker>
+        <Text style={styles.TextInformativo}>Perído</Text>
+        <Picker
+          selectedValue={selectedValue}
+          style={styles.SelectPicker}
+          onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
+          mode="dropdown">
+          <Picker.Item label="-" value="" />
+          <Picker.Item label="MANHÃ" value="MANHÃ" />
+          <Picker.Item label="TARDE" value="TARDE" />
+          <Picker.Item label="NOITE" value="NOITE" />
+        </Picker>
 
-      <Text style={styles.TextInformativo}>Condições da Área</Text>
-      <Picker
-        selectedValue={selectedValue2}
-        style={styles.SelectPicker}
-        onValueChange={(v) => setSelectedValue2(v)}
-        mode="dropdown">
-        <Picker.Item label="-" value="" />
-        <Picker.Item label="OPERÁVEL" value="OPERÁVEL" />
-        <Picker.Item label="OPERÁVEL PARC." value="OPERÁVEL PARC." />
-        <Picker.Item label="INOPERÁVEL" value="ONOPERÁVEL" />
-      </Picker>
+        <Text style={styles.TextInformativo}>Condições da Área</Text>
+        <Picker
+          selectedValue={selectedValue2}
+          style={styles.SelectPicker}
+          onValueChange={(v) => setSelectedValue2(v)}
+          mode="dropdown">
+          <Picker.Item label="-" value="" />
+          <Picker.Item label="OPERÁVEL" value="OPERÁVEL" />
+          <Picker.Item label="OPERÁVEL PARC." value="OPERÁVEL PARC." />
+          <Picker.Item label="INOPERÁVEL" value="ONOPERÁVEL" />
+        </Picker>
 
-      <TouchableOpacity
-        style={styles.buttonToucha}
-        onPress={handleSubmit(onSubmit)}>
-        <Text style={styles.TextButton}>Enviar</Text>
-      </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.buttonToucha}
+          onPress={handleSubmit(onSubmit)}>
+          <Text style={styles.TextButton}>Enviar</Text>
+        </TouchableOpacity>
+      </ScrollView>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   backgroud: {
+    marginHorizontal: 10,
     flex: 1,
     backgroundColor: '#0000',
     justifyContent: 'center',
   },
   containerLogo: {
-    backgroundColor: '#dddd',
-    height: 300,
-    marginTop: 20,
-    marginVertical: 30,
-    flex: 1,
+    backgroundColor: '#0000',
+    height: 130,
+    marginTop: 0,
+    alignItems: 'center',
     justifyContent: 'center',
   },
   textCadastro: {
@@ -190,7 +219,7 @@ const styles = StyleSheet.create({
     width: '90%',
     marginBottom: 10,
     backgroundColor: '#fff',
-    borderRadius: 15,
+    borderRadius: 10,
     fontSize: 17,
   },
   MultiSelect: {
